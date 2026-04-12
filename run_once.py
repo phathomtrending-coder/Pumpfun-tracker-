435b1bde2e10779ce780bdbc600ccd9df0de9a3d
phathomtrending-coder <phathomtrending@gmail.com>
2026-04-12 17:15:52 -0400

    Remove post_test_message call from main function
    Removed the call to post_test_message in the main function.

--- run_once.py
+++ run_once.py
@@ -1,10 +1,8 @@
 import asyncio
 
-from posters import post_test_message
 from bot import scan_for_new_tokens, check_multipliers
 
 async def main():
-    await post_test_message()
     await scan_for_new_tokens()
     await check_multipliers()
 
