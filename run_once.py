ed7f8fa8eb99a10de6975ab811ffe727a88647cd
phathomtrending-coder <phathomtrending@gmail.com>
2026-04-12 23:17:28 -0400

    Replace check_multipliers with check_updates

--- run_once.py
+++ run_once.py
@@ -1,10 +1,9 @@
 import asyncio
-
-from bot import scan_for_new_tokens, check_multipliers
+from bot import scan_for_new_tokens, check_updates
 
 async def main():
     await scan_for_new_tokens()
-    await check_multipliers()
+    await check_updates()
 
 if __name__ == "__main__":
     asyncio.run(main())
